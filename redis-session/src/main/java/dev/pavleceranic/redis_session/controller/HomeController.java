55623751767c85d40822d63729dd66311b2eeadc
pavleCeranic <pavle.ceranic@develabs.net>
2024-07-10 11:00:45 +0200

    redis session example

---
+++ redis-session/src/main/java/dev/pavleceranic/redis_session/controller/HomeController.java
@@ -0,0 +1,35 @@
+package dev.pavleceranic.redis_session.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+
+    private final String HOME_VIEW_COUNT = "HOME_VIEW_COUNT";
+    @GetMapping("/")
+    public String home(Principal principal, HttpSession session) {
+
+        incremenCount(session, HOME_VIEW_COUNT);
+        return "Hello" + principal.getName();
+    }
+
+    @GetMapping("/count")
+    public String count(HttpSession session){
+        return "HOME_VIEW_COUNT: " + session.getAttribute(HOME_VIEW_COUNT);
+    }
+
+
+    private void incremenCount(HttpSession session, String attribute) {
+
+        var homeViewCount = session.getAttribute(attribute) == null ? 0 : (Integer) session.getAttribute(attribute);
+
+        session.setAttribute(attribute, homeViewCount+=1);
+
+    }
+
+}
